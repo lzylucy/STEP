8f3a578c822f01cb9f0a44bf0f47161b8c1a8d5e
Zeyi Liu <lucialiu814@gmail.com>
2020-06-18 14:47:21 +0000

    remove import Message

--- portfolio/src/main/java/com/google/sps/servlets/NewCommentServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/NewCommentServlet.java
@@ -23,7 +23,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
-import com.google.sps.data.Message;
 import java.util.ArrayList;
 
 /** Servlet that stores new comments. */
