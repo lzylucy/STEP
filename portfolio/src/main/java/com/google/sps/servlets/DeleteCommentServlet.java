93c55cc2650691ea2ede8c61eda19af601891659
Zeyi Liu <lucialiu814@gmail.com>
2020-06-19 02:16:26 +0000

    modify DeleteCommentServlet to delete all comments at a time

--- portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
@@ -16,24 +16,31 @@ package com.google.sps.servlets;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet responsible for deleting tasks. */
+/** Servlet responsible for deleting comments. */
 @WebServlet("/delete-data")
 public class DeleteCommentServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    long id = Long.parseLong(request.getParameter("id"));
-
-    Key messageEntityKey = KeyFactory.createKey("Message", id);
+    Query query = new Query("Message");
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    datastore.delete(messageEntityKey);
+    PreparedQuery results = datastore.prepare(query);
+
+    for (Entity entity : results.asIterable()) {
+      long id = entity.getKey().getId();
+      Key messageEntityKey = KeyFactory.createKey("Message", id);
+      datastore.delete(messageEntityKey);
+    }
   }
 }
