f86092c233408bd7e19a5ca74bd6e302b02f1d28
Zeyi Liu <lucialiu814@gmail.com>
2020-06-27 08:08:58 +0000

    modify frontend to get user location

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -141,7 +141,25 @@ function fetchBlobstoreUrlAndShowForm() {
     });
 }
 
+/** Obtains user's geo location and adds the info to form  */
+function getUserGeoInfo() {
+  var geoSuccess = function(position) {
+    document.getElementById('lat').value = position.coords.latitude;
+    document.getElementById('long').value= position.coords.longitude;
+  };
+  var geoError = function(error) {
+    console.log('Error occurred. Error code: ' + error.code);
+    // error.code can be:
+    //   0: unknown error
+    //   1: permission denied
+    //   2: position unavailable (error response from location provider)
+    //   3: timed out
+  };
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+}
+
 window.onload = function() {
   loadComments();
   fetchBlobstoreUrlAndShowForm();
+  getUserGeoInfo();
 }
