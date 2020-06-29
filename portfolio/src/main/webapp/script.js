3026e39cf6ed1ec17a5e09c21dbc512596fbb87c
Zeyi Liu <lucialiu814@gmail.com>
2020-06-29 06:02:00 +0000

    reformat code

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -148,12 +148,12 @@ function getUserGeoInfo() {
     document.getElementById('long').value= position.coords.longitude;
   };
   var geoError = function(error) {
-    console.log('Error occurred. Error code: ' + error.code);
     // error.code can be:
     //   0: unknown error
     //   1: permission denied
     //   2: position unavailable (error response from location provider)
     //   3: timed out
+    console.log('Error occurred. Error code: ' + error.code);
   };
   navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
 }
