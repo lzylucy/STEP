ec97a2b4ea401ffe9ff0780037392858f0cd3a3e
Zeyi Liu <lucialiu814@gmail.com>
2020-06-30 06:29:13 +0000

    implement query()

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,44 +14,90 @@
 
 package com.google.sps;
 
+import com.google.sps.Event;
+import com.google.sps.TimeRange;
+import com.google.sps.MeetingRequest;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    Collection<TimeRange> result = new ArrayList<>();
+    ArrayList<TimeRange> unavailable = new ArrayList<>();
     for (String attendee: request.getAttendees()) {
-      Collection<TimeRange> meetingTimes = getMeetingTimes(events, attendee);
-      merge(result, meetingTimes);
+      ArrayList<TimeRange> meetingTimes = getMeetingTimes(events, attendee);
+      unavailable = merge(unavailable, meetingTimes);
+    }
+
+    Collection<TimeRange> result = new ArrayList<>();
+    for (TimeRange t: getAvailability(unavailable)) {
+      if (t.duration() >= request.getDuration()) {
+        result.add(t);
+      }
     }
 
     return result;
   }
 
-  private void merge(Collection<TimeRange> result, Collection<TimeRange> other) {
+  private ArrayList<TimeRange> merge(ArrayList<TimeRange> result, ArrayList<TimeRange> other) {
     // If result is empty, add other to the result directly
+    // If other is empty, do nothing and return result
     // If result is not empty, need to merge the two, keep the overlapping ranges
-    
+    if (result.isEmpty()) {
+      return other;
+    }
+    if (other.isEmpty()) {
+      return result;
+    }
 
+    result.addAll(other);
+    Collections.sort(result, TimeRange.ORDER_BY_START);
+
+    for (int i = 0; i < result.size()-1; i++) {
+      TimeRange current = result.get(i);
+      TimeRange next = result.get(i+1);
+      if (current.overlaps(next)) {
+        // Case 1:  |_______|
+        //            |__|
+        if (current.contains(next)) {
+          result.remove(next);
+          i--;
+        }
+        // Case 2:  |______|
+        //               |______|
+        else {
+          result.set(i, TimeRange.fromStartEnd(current.start(), next.end(), false));
+          result.remove(next);
+          i--;
+        }
+      }
+    }
+    
+    return result;
   }
 
-  private Collection<TimeRange> getMeetingTimes(Collection<Event> events, String attendee) {
+  private ArrayList<TimeRange> getMeetingTimes(Collection<Event> events, String attendee) {
     // Get attendee's unavailable time ranges
     // Assume required meetings for one person do not overlap
-    Collection<TimeRange> meetingTimes = new ArrayList<>();
+    ArrayList<TimeRange> meetingTimes = new ArrayList<>();
     for (Event e: events) {
       if (e.getAttendees().contains(attendee)) {
         meetingTimes.add(e.getWhen());
       }
     }
-    // Collection<TimeRange> availability = new ArrayList<>();
-    // int point = TimeRange.START_OF_DAY;
-    // for (TimeRange t: meetingTime) {
-    //   availability.add(TimeRange.fromStartEnd(point, t.start(), false));
-    //   point = t.end();
-    // }
-    // availability.add(TimeRange.fromStartEnd(point, TimeRange.END_OF_DAY));
 
     return meetingTimes;
+  }
+
+  private Collection<TimeRange> getAvailability(Collection<TimeRange> unavailable) {
+    Collection<TimeRange> availability = new ArrayList<>();
+    int point = TimeRange.START_OF_DAY;
+    for (TimeRange t: unavailable) {
+      availability.add(TimeRange.fromStartEnd(point, t.start(), false));
+      point = t.end();
+    }
+    availability.add(TimeRange.fromStartEnd(point, TimeRange.END_OF_DAY, true));
+
+    return availability;
   }
 }
